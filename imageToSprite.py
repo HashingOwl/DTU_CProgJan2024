330e9a5a22b8aeccff9757ae824d0eb781a4be0c
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2024-01-15 16:15:46 +0100

    Sprite generation update

--- imageToSprite.py
+++ imageToSprite.py
@@ -1,20 +1,21 @@
 import numpy as np
 from PIL import Image
 
-imgName = "C:\\Users\\alber\\OneDrive\\Dokumenter\\GitHub\\DTU_CProgJan2024\\test.png"
-spriteName = "spriteTest"
+imgName = "C:\\Users\\alber\\Downloads\\BG1.png"
+spriteName = "testBG2"
 image_file = Image.open(imgName)
 image_file = image_file.convert('1')
 
 imageMatrix = np.asarray(image_file)
 print(imageMatrix.shape)
 sprite = []
-countLength = 16-1
-count = countLength
+countLength = 8-1
+count = 0
 numb = 0
 icount = 0
-for i in range(imageMatrix.shape[0]//2):
-    for j in range(imageMatrix.shape[1]):
+"""
+for j in range(imageMatrix.shape[1]):
+    for i in range(imageMatrix.shape[0]//2):
         if count == -1:
             sprite.append(numb)
             numb = 0
@@ -24,12 +25,29 @@ for i in range(imageMatrix.shape[0]//2):
         if imageMatrix.shape[0] > i*2+1:
             numb += (imageMatrix[i*2+1][j]<<count)
         count -= 1
-sprite.append(numb)
+sprite.append(numb)"""
+
+for y in range(imageMatrix.shape[0]//2):
+    for x in range(imageMatrix.shape[1]):
+        numb += (imageMatrix[y*2][x]<<count)
+        count += 1
+        if y*2+1<imageMatrix.shape[0]:
+            numb += (imageMatrix[y*2+1][x]<<count)
+        count += 1
+        if count >= countLength:
+            sprite.append(numb)
+            numb = 0
+            count = 0
 print("uint8_t {}x = {};".format(spriteName,imageMatrix.shape[1]))
 print("uint8_t {}y = {};".format(spriteName,imageMatrix.shape[0]))
-outString = "uint16_t {}[{}] = ".format(spriteName,len(sprite)) + "{"
+outString = "uint8_t {}[{}] = ".format(spriteName,len(sprite)) + "{"
+count = 0
 for i in sprite:
-    outString += str(hex(i & 0xffffffff)) + ","
+    outString += "0x"+f"{(i & 0xffffffff):02x}" + "," +"\t"
+    count += 1
+    if count == 16:
+        count = 0
+        outString += "\n"
 outString += "};"
 
 print(outString)
