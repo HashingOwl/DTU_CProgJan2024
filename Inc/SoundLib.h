a0437d0c084511ad818f4d4426833ed4f467ef1f
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2024-01-16 13:32:03 +0100

    Music h file fix

--- Inc/SoundLib.h
+++ Inc/SoundLib.h
@@ -17,20 +17,21 @@
  // Whatever needs to be defined
 /* Exported functions -------------------------------------------------- */
 //New system
-/*
+
 void soundInit(void);
 void TIM2_IRQHandler(void);
 void TIM1_UP_TIM16_IRQHandler(void);
-*/
+void disableMusic();
+void ensableMusic();
 
 //old system
-
+/*
 void soundInit(void);
 void TIM2_IRQHandler(void);
 void TIM1_UP_TIM16_IRQHandler(void);
 void setFreqARR(uint16_t ARRval);
 void updateMusic();
-void disableMusic();
-extern volatile uint8_t musicUpdateFlag;
 
+extern volatile uint8_t musicUpdateFlag;
+*/
 #endif
